c95c63251790fcddc218f7c3aa75951cbf1b79c5
NanZ-hash <afnanxmt@gmail.com>
2021-08-24 07:48:03 +0300

    Adding the numbers and the operation button to the UI

--- README.md
+++ README.md
@@ -6,9 +6,9 @@
 I started this small project to refresh my programing skills with react JS, and I like to see it where can I extend it.
 for a start : 
 - The calculator will do simple function of { + , - , / , * }.
-- The user can press a button. 
-
-
+- The user can press a button with a number and it will show on the display;
+- The user can press a button with an operation and it will function, but will not show ( for the time being ).
+- when the user press the = operation it will show the result.
 ---
 Futer Goal:
 1. This calculator will cal will have more advanced functions,
--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
-function App() {
+const App = () => {
+
+  // setting up the values for the calculator
+  const [result, setReslut]= useState(""); 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         HI 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* cla body  */}
+  <div className="container"> 
+  {/* to display the result  */}
+  <form > 
+    <input type="text" value={result} /> 
+  </form>
+
+  <div className="keyboard" />
+  <button>Clear </button>
+  <button>&divide;</button>
+  <button>7</button>
+  <button>8</button>
+  <button>9</button>
+  <button>&times;</button>
+  <button>4</button>
+  <button>5</button>
+  <button>6</button>
+  <button>&ndash;</button>
+  <button>1</button>
+  <button>2</button>
+  <button>3</button>
+  <button>+</button>
+  <button>0</button>
+  </div>
+    </>
   );
 }
-
 export default App;
