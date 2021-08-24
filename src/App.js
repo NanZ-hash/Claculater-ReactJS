e8a4c9cd19f9f1494ba4d9c16d22b90a9291ccd7
NanZ-hash <afnanxmt@gmail.com>
2021-08-24 08:05:43 +0300

    updating App.css file for the calculator UI to be simple

--- src/App.js
+++ src/App.js
@@ -10,26 +10,26 @@ const App = () => {
     {/* cla body  */}
   <div className="container"> 
   {/* to display the result  */}
-  <form > 
+    <form > 
     <input type="text" value={result} /> 
   </form>
-
-  <div className="keyboard" />
-  <button>Clear </button>
-  <button>&divide;</button>
-  <button>7</button>
-  <button>8</button>
-  <button>9</button>
-  <button>&times;</button>
-  <button>4</button>
-  <button>5</button>
-  <button>6</button>
-  <button>&ndash;</button>
-  <button>1</button>
-  <button>2</button>
-  <button>3</button>
-  <button>+</button>
-  <button>0</button>
+    <div className="keyboard" >
+    <button>Clear </button>
+    <button>&divide;</button>
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    <button>&times;</button>
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button>&ndash;</button>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>+</button>
+    <button>0</button>
+    </div>
   </div>
     </>
   );
